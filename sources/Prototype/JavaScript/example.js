3ba5b654d47a80d5e0a41ec5190764734e42a030
Dmitry Sheiko <web.sbelarus@gmail.com>
2013-04-08 13:04:16 +0200

    Prototype docs in progress

--- sources/Prototype/JavaScript/example.js
+++ sources/Prototype/JavaScript/example.js
@@ -6,16 +6,7 @@
  * @jscs standard:Jquery
  * Code style: http://docs.jquery.com/JQuery_Core_Style_Guidelines
  * 
- * In this application we need a button to submit the form (Save changes) 
- * and a button to preserve the form data (Save draft). The framework comprises 
- * an object representing an abstract button (Toolbar.Button). The object's 
- * constructor receives configuration object as an argument (object specifiers). Thus, any instance
- * of Toolbar.Button makes a configured button. Any button meant for saving has 
- * a particular appearence. In Twitter Bootstrap 2 it is gained by adding class
- * btn-primary. So, we clone default button object and add the class to it. 
- * Save changes button and Save draft button differ only by onClick event 
- * handlers. In everything else they are the same as default save button. So 
- * we can clone saveBtn object and, then, assign the handler to each.    
+   
  */
 
 (function( window ){
--- sources/Prototype/PHP/tutorial.php
+++ sources/Prototype/PHP/tutorial.php
@@ -6,12 +6,6 @@
  * @licence MIT
  */
 
-/*
- Let's assume, we deal with micro-formats in our application. The application is built on the framework, which already has base class (Element) for HTML element objects. Thus, every element makes an instance of this class.
-Micro-format elements are HTML elements, which are marked in special way by the class attribute. So, to get them, we could subclass Element. However, that would produce a lot of subclasses, which differ only by the micro-format type the derived objects represent. Besides, it would be inconsistent to the framework design.
-How do we solve it? Instead of generalization we will use composition. We create an HTML element object by instantiating Element class.  We pass to the constructor tag name to specify which exact element it is. Now we have a state-full object, which we clone every time we need a new a micro-format element object.
- *
-*/
 namespace Framework\Html;
 
 final class Element
